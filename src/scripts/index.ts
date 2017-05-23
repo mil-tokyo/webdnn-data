2182ddeb0e2bea496c37a84a94bbe779e0afe41f
Kikurage <y.kikura@gmail.com>
2017-05-23 18:00:22 +0900

    #1 add compatibility table

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,1 +1,21 @@
 import "../style/index.scss";
+
+function initialize() {
+    let availability = WebDNN.getBackendAvailability();
+
+    for (let backend of ['webgpu', 'webassembly', 'fallback']) {
+        for (let node of document.querySelectorAll(`.Compatibility-${backend}.Compatibility-checking`)) {
+            node.classList.remove('Compatibility-checking');
+            let statusNode = node.querySelector('.Compatibility-Status');
+
+            if (availability.status[backend]) {
+                node.classList.add('Compatibility-supported');
+                if (statusNode) statusNode.textContent = 'Supported';
+            } else {
+                node.classList.add('Compatibility-not_supported');
+                if (statusNode) statusNode.textContent = 'Not supported';
+            }
+        }
+    }
+}
+window.onload = initialize;
